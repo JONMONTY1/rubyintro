bec5a2ef70e3059afaad56d3e1fa6fb7355ec480
Jonathan Monty <jonathan.monty@kellogg.northwestern.edu>
2023-05-07 04:40:30 +0000

    loops

--- code-along/4-hashes.rb
+++ code-along/4-hashes.rb
@@ -4,6 +4,37 @@
 
 # Hashes are lists of *key-value pairs*
 
+favorite_things = { "color" => "purple", "number" => 17, "computer" => "Apple" }
+puts favorite_things
+
+user = {
+  "name" => "Ben",
+  "location" => { "city" => "Chicago", "state" => "IL" },
+  "status" => "Staying warm!"
+}
+
 # Accessing data from the hash
+
+name = user["name"]
+puts name
+
+city = user["location"]["city"]
+puts city
+
+state = user["location"]["state"]
+puts state
+
+user = {
+  "name" => { "first" => "Ben", "last" => "Block" },
+  "location" => { "city" => "Chicago", "state" => "IL" },
+  "timeline" => [{ "status" => "Brrr!", "posted_at" => "9:00am" },
+                 { "status" => "Coding.", "posted_at" => "10:00am" },
+                 { "status" => "Lunch time.", "posted_at" => "12:00pm" }]
+}
+
+first_name = user["name"]["first"]
+first_status = user["timeline"][0]["status"]
+first_status_time = user["timeline"][0]["posted_at"]
+puts "#{first_name}'s first post was #{first_status} at #{first_status_time}"
 
 # More Complex Hashes
--- code-along/5-loops.rb
+++ code-along/5-loops.rb
@@ -2,6 +2,17 @@
 # is the same as where this file is located and then run:
 # ruby 5-loops.rb
 
+friends = ['Rachel', 'Monica', 'Phoebe', 'Ross', 'Chandler', 'Joey']
+
+greeting = "Hi #{friends[0]}"
+puts greeting
+
+
+for friend in friends
+    greeting = "Hi #{friend}"
+    puts greeting
+  end
+
 # Infinite Tacos -> Finite Tacos
 # loop do
 #   puts "tacos!"
