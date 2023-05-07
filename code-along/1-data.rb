1a14a59dab75565077e7ab10d0b0c591dc34ece8
Jonathan Monty <jonathan.monty@kellogg.northwestern.edu>
2023-05-07 03:14:56 +0000

    string manipulation

--- code-along/1-data.rb
+++ code-along/1-data.rb
@@ -21,9 +21,37 @@ puts "Hello, world!"
 # Combine strings together
 
 puts "ruby is " + "fun!"
+puts "tacos" * 3
 
 # Variables
 
+x = 2
+y = 5
+puts x
+puts y
+
+food = "tacos"
+quantity = 3
+puts food * quantity
+
 # Combine strings and variables
 
+puts "tacos: " + 3.to_s
+
 # String manipulation
+
+first_name = "Mando"
+greeting = "Hello, #{first_name}!"
+puts greeting
+
+puts "#{food}: #{quantity}"
+
+puts "Hello      " + "there!"
+puts "Hello".length
+puts "how are you today?".capitalize
+puts "computer".reverse
+
+creed = "This Is The Way"
+puts creed.upcase
+puts creed.downcase
+puts creed.swapcase
